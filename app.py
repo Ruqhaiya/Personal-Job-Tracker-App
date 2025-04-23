24bd19b1579d3796a1d3d023ba5ed7b85b3deed6
Ruqhaiya <syedaruqhaiyafatima@gmail.com>
2025-04-23 03:53:56 -0700

    .

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ authenticator = stauth.Authenticate(
 # Attempt login
 # login_info = authenticator.login("Login", location="main")
 
-login_info = authenticator.login("Login", location="main")
+login_info = authenticator.login("Login", "main")
 
 if login_info:
     name, auth_status, username = login_info
